04d79fd118b673df2fcd2b2b5bf55ae1868a5e67
caizhibang <caizhibang@kunteng.org>
2019-03-25 10:49:38 +0800

    caizhibang:20190325 add support redis

--- src/main/java/com/kunteng/cyria/CyriaApplication.java
+++ src/main/java/com/kunteng/cyria/CyriaApplication.java
@@ -2,8 +2,10 @@ package com.kunteng.cyria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CyriaApplication {
 
 	public static void main(String[] args) {
